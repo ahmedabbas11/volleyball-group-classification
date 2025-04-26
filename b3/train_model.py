c8cba10476aecadc03fb16d3f0dfe3365d0db88f
Ahmed Abbas <ahmedabbas11@gmail.com>
2025-04-26 09:14:04 +0300

    introduce checkpoints

--- b3/train_model.py
+++ b3/train_model.py
@@ -2,13 +2,15 @@
 import torch
 import torch.nn as nn
 from torchvision import models
-
+from answers.volleyball_annot_loader import working_dir
 from b3.volleyball_action_dataloader import data_loader
 
 # Hyperparameters
 batch_size = 128
 num_epochs = 5
 learning_rate = 0.0001
+snapshot_dir = f'{working_dir}/snapshots'
+os.makedirs(snapshot_dir, exist_ok=True)
 
 def train():
     # Load data
@@ -27,6 +29,19 @@ def train():
     # Loss function and optimizer
     criterion = nn.CrossEntropyLoss()  # For classification problems
     optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+    start_step = 0
+    snapshot_path = 'snapshots/snapshot_latest.pt'
+
+    if os.path.exists(snapshot_path):
+        checkpoint = torch.load(snapshot_path)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+        start_step = checkpoint['step'] + 1
+        print(f"Resumed from step {start_step}")
+    else:
+        print("Starting from scratch.")
 
     # Training loop
     print('training started')
@@ -54,6 +69,20 @@ def train():
             if i % 20 == 19:  # Print every 200 mini-batches
                 print(f'[Epoch {epoch + 1}, Batch {i + 1}] loss: {running_loss / 20:.3f}')
                 running_loss = 0.0
+            if i % 1000 == 0:
+                snapshot_path = os.path.join(snapshot_dir, f'snapshot_step_{i}.pt')
+                torch.save({
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'step': i,
+                }, snapshot_path)
+                print(f"Saved snapshot: {snapshot_path}")
+                # Save snapshot again
+                torch.save({
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'step': step,
+                }, 'snapshots/snapshot_latest.pt')
 
     print('Finished Training')
 
